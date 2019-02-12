0614bb50847d544754964a79d98500f46714d68f
Incapacitate <lhdylan@hotmail.com>
2019-02-12 23:07:05 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/secret'
   devise_for :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
