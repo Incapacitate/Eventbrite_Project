ac3eb65ca51e7734d524873dec9c238213ed2642
Incapacitate <lhdylan@hotmail.com>
2019-02-13 18:17:47 +0100

    up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+i = 1
+10.times do
+  User.create!(password: "lollol", email: Faker::Internet.email, first_name: "blabla#{i}")
+
+  i+=1
+
+end
+puts "Profil créé"
+
+10.times do
+Event.create!(start_date: Faker::Date.forward(23), duration: "60", user_id: rand(1..User.count), price: rand(6..666), location: Faker::Address.city, title: Faker::Address.community, description: Faker::ChuckNorris.fact)
+end
+puts "Event créé"
+
+20.times do
+Attendance.create!(user_id: rand(1..User.count), event_id: rand(1..User.count))
+
+end
+puts "Participation ok"
